18766fa417c832f6eda3aa4d2ff6e9b9e361ceff
Yeonjae Kim <duswokim1220@gmail.com>
2024-03-24 12:12:13 +0300

    d31 234 except sec10 challange

---
+++ 6_JS_learning/app.js
@@ -0,0 +1,19 @@
+let age = 24;
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
+};
+
+alert(hobbies[0]);
+
+//문자열, 숫자 변수 설명
+// let age = 24;
+// let greetingText = "Hi I'm yeonjae!";
+// alert(greetingText);
+// greetingText = "i'm really yeonjae";
+// alert(greetingText);
+// alert(age);
