1c7da5999a04619bc379d8bf0de5232b91c7afc4
Yeonjae Kim <duswokim1220@gmail.com>
2024-04-02 21:46:23 +0300

    d45 328

--- 10_tic_tac_toe/scripts/game.js
+++ 10_tic_tac_toe/scripts/game.js
@@ -1,43 +1,128 @@
-function startNewgame() {
-    if (players[0].name === '' || players[1].name === '') {
-        alert('Please set custom player names for both players!');
-        return
+function resetGameStatus() {
+    activePlayer = 0;
+    currentRound = 1;
+    gameIsOver = false;
+    gameOverElement.firstElementChild.innerHTML = 'You won, <span id="winner-name">PLAYER NAME</span>!';
+    gameOverElement.style.display = 'none';
+
+    let gameBoardIndex = 0;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            gameData[i][j] = 0;
+            const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+            gameBoardItemElement.textContent = '';
+            gameBoardItemElement.classList.remove('disabled')
+            gameBoardIndex++;
+        }
     }
-    
-    activePlayerNameElement.textContent = players[activePlayer].name
-    gameAreaElement.style.display = 'block';
+}
+
+function startNewgame() {
+  if (players[0].name === "" || players[1].name === "") {
+    alert("Please set custom player names for both players!");
+    return;
+  }
+
+  resetGameStatus();
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = "block";
 }
 
 function switchPlayer() {
-    if (activePlayer === 0) {
-        activePlayer = 1;
-    } else {
-        activePlayer = 0;
-    }
-    activePlayerNameElement.textContent = players[activePlayer].name;
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
 function selectGameField(event) {
-    // console.log(event.target.tagName);
-    if (event.target.tagName !== 'LI') {
-        return
+  // console.log(event.target.tagName);
+  if (event.target.tagName !== "LI" || gameIsOver) {
+    return;
+  }
+
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1; // 수학 연산이 들어가있기에 문자열 앞에 + 붙일 필요는 없다
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedColumn][selectedRow] > 0) {
+    alert("Please select an empty field!");
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedColumn][selectedRow] = activePlayer + 1;
+  // console.log(gameData);
+  const winnerID = checkForGameOver();
+  //   console.log(winnerID);
+
+  if (winnerID !== 0) {
+    endGame(winnerID);
+  }
+
+  currentRound++;
+  switchPlayer();
+}
+
+function checkForGameOver() {
+  // Checking the rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
     }
-
-    
-    const selectedField = event.target;
-    const selectedColumn = selectedField.dataset.col - 1; // 수학 연산이 들어가있기에 문자열 앞에 + 붙일 필요는 없다
-    const selectedRow = selectedField.dataset.row - 1;
-
-    if(gameData[selectedColumn][selectedRow] > 0) {
-        alert('Please select an empty field!')
-        return;
+  }
+  // Checking the columns for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
     }
+  }
+  // Diagonal: Top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
 
-    selectedField.textContent = players[activePlayer].symbol;
-    selectedField.classList.add('disabled');
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
 
-    gameData[selectedColumn][selectedRow] = activePlayer + 1;
-    console.log(gameData);
+  if (currentRound === 9) {
+    return -1;
+  }
 
-    switchPlayer();
+  return 0;
+}
+
+function endGame(winnerID) {
+  gameOverElement.style.display = "block";
+
+  if (winnerID > 0) {
+    gameIsOver = true;
+    const winnerName = players[winnerID - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw!";
+  }
 }
